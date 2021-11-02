036a5060c3244fe77d89cba32d808deb6561cd04
yemreu <yunusemreuzun@outlook.com>
2021-11-02 22:20:54 +0300

    Day 2 - JavaScript basics

---
+++ javaScriptStarterKit/src/functions.js
@@ -0,0 +1,24 @@
+function addToCart(quantity,productName = "Elma") {
+    console.log("Sepete eklendi! ürün: " + productName + " adet: " + quantity);
+}
+
+//addToCart();
+addToCart(10);
+//addToCart(15);
+
+
+let dolarDun = 9.50
+
+let sayHello = () => {
+    console.log("Hello world");
+}
+
+sayHello();
+
+let sayHello2 = function(){
+    console.log("Hello world 2");
+}
+
+sayHello2();
+
+//58:18
