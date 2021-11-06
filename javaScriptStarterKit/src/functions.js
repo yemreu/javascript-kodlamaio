02429b0cf627ebc5f840c172b516928a52c84369
yemreu <yunusemreuzun@outlook.com>
2021-11-06 22:36:04 +0300

    Day 2 - JavaScript basics, functions..

--- javaScriptStarterKit/src/functions.js
+++ javaScriptStarterKit/src/functions.js
@@ -21,4 +21,41 @@ let sayHello2 = function(){
 
 sayHello2();
 
-//58:18
+function addToCart2(productName,quantity,unitPrice) {
+    
+}
+
+addToCart2("Elma",5,10);
+addToCart2("Armut",2,20);
+addToCart2("Elma",3,15);
+
+
+let product1 = {productName:"Elma",unitPrice:10,quantity:5};
+
+function addToCart3(product){
+    console.log("Ürün: " + product.productName);
+    console.log("Adet: " + product.quantity);
+    console.log("Fiyat: " + product.unitPrice);
+}
+
+addToCart3(product1);
+
+let product2 = {productName:"Elma",unitPrice:10,quantity:5};
+let product3 = {productName:"Elma",unitPrice:10,quantity:5};
+product2 = product3;
+product2.productName = "Karpuz";
+console.log(product3.productName);
+
+function addToCart4(products) {
+    console.log(products);
+}
+
+let products = [
+    {productName:"Elma",unitPrice:10,quantity:5},
+    {productName:"Elma",unitPrice:10,quantity:5},
+    {productName:"Elma",unitPrice:10,quantity:5}
+];
+
+addToCart4(products);
+
+//1:58:27
