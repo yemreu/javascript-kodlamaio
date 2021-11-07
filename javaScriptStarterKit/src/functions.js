c3104902b3b38e69b43fec2216b8102bac5c0ecf
yemreu <yunusemreuzun@outlook.com>
2021-11-07 23:39:25 +0300

    Day 2 - JavaScript basics, rest, spread, destructuring

--- javaScriptStarterKit/src/functions.js
+++ javaScriptStarterKit/src/functions.js
@@ -58,4 +58,42 @@ let products = [
 
 addToCart4(products);
 
-//1:58:27
+//rest
+function add(...numbers){
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i];
+    }
+    console.log(total);
+} 
+
+add(20,30);
+add(20,30,40);
+add(20,30,40,50);
+
+//spread
+let numbers = [30,10,500,600,120];
+console.log(Math.max(...numbers));
+
+//destructuring
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
+    {name:"İç Anadolu",population:"20M"},
+    {name:"Marmara",population:"30M"},
+    {name:"Karadeniz",population:"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Sinop","Trabzon"],
+    ]
+];
+
+console.log(icAnadolu.name);
+console.log(icAnadolu.population);
+console.log(icAnadoluSehirleri);
+let newProductName,newUnitPrice,newQuantity
+({productName:newProductName,unitPrice:newUnitPrice,quantity:newQuantity} = {productName:"Elma",unitPrice:10,quantity:5});
+
+console.log(newProductName);
+console.log(newUnitPrice);
+console.log(newQuantity);
+
