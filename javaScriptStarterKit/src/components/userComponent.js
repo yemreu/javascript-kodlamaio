cc2071af2c114c978aa76d703c115b35c92dcc13
yemreu <yunusemreuzun@outlook.com>
2021-11-16 21:15:45 +0300

    Day 3 - JavaScript ES5+ & OOP

---
+++ javaScriptStarterKit/src/components/userComponent.js
@@ -0,0 +1,23 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
+import User from "../models/user.js";
+import UserService from "../services/userService.js";
+
+console.log("User component yüklendi.");
+
+let logger1 = new MongoLogger();
+let userService = new UserService(logger1);
+
+let user1 = new User(1,"Engin","Demiroğ","Ankara");
+let user2 = new User(2,"Emre","Uzun","Samsun");
+
+userService.add(user1);
+userService.add(user2);
+console.log(userService.getById(1));
+console.log(userService.getById(2));
+console.log(userService.list());
+
+
+
+let customer = {id:1,firstName:"Engin"}
+customer.lastName = "Demiroğ";
+console.log(customer.lastName);
